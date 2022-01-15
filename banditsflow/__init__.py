24a6a355d5b24b067488e147870dfe77b8d47588
monochromegane <dev.kuro.obi@gmail.com>
2022-01-15 11:03:14 +0900

    List steps of banditsflow.

--- banditsflow/__init__.py
+++ banditsflow/__init__.py
@@ -1,9 +1,28 @@
-from metaflow import FlowSpec, step
+from metaflow import FlowSpec, Parameter, step
+from metaflow.datastore.inputs import Inputs
 
-class BanditsFlow(FlowSpec): # type: ignore
+
+class BanditsFlow(FlowSpec):  # type: ignore
+    param_actor = Parameter("actor", type=str, multiple=True, help="Name of actor")
 
     @step
     def start(self) -> None:
+        self.next(self.optimize, foreach="param_actor")
+
+    @step
+    def optimize(self) -> None:
+        self.next(self.evaluate)
+
+    @step
+    def evaluate(self) -> None:
+        self.next(self.join)
+
+    @step
+    def join(self, inputs: Inputs) -> None:
+        self.next(self.plot)
+
+    @step
+    def plot(self) -> None:
         self.next(self.end)
 
     @step
