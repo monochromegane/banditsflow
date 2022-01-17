8705c6a471bae366fbf22c5afe8bdaeadb88ef05
monochromegane <dev.kuro.obi@gmail.com>
2022-01-17 17:25:34 +0900

    Add tests for run of simulator.

--- tests/test_simulator.py
+++ tests/test_simulator.py
@@ -1,4 +1,4 @@
-from unittest.mock import Mock, call
+from unittest.mock import Mock, call, patch
 
 from banditsflow import actor as act
 from banditsflow import scenario
@@ -52,16 +52,44 @@ def test_callbacks_are_called_with_parameters_in_run_scenario() -> None:
     callback_0 = Mock()
     callback_1 = Mock()
     current_ite = 0
-    result = simulator._run_scenario(
-        current_ite, "", "", {}, [callback_0, callback_1], 0
-    )
+    _ = simulator._run_scenario(current_ite, "", "", {}, [callback_0, callback_1], 0)
 
     assert callback_0.call_count == 2
-    list_args = callback_0.call_args_list
-    for i in range(2):
-        assert call(current_ite, i, {"metric": {"i": i}, "result": {"i": i}})
+    args_list = callback_0.call_args_list
+    for i, args in enumerate(args_list):
+        assert args == call(current_ite, i, {"metric": {"i": i}, "result": {"i": i}})
 
     assert callback_1.call_count == 2
-    list_args = callback_1.call_args_list
-    for i in range(2):
-        assert call(current_ite, i, {"metric": {"i": i}, "result": {"i": i}})
+    args_list = callback_1.call_args_list
+    for i, args in enumerate(args_list):
+        assert args == call(current_ite, i, {"metric": {"i": i}, "result": {"i": i}})
+
+
+def test_run_returns_n_ite_results() -> None:
+    simulator = sim.Simulator(DummyScenarioLoader, DummyEchoActorLoader)
+    n_ite = 3
+    results = simulator.run(n_ite, "", "", {}, [], 0)
+    assert len(results) == n_ite
+
+
+def test_run_scenario_is_called_n_ite_times_with_parameters_in_run() -> None:
+    simulator = sim.Simulator(DummyScenarioLoader, DummyEchoActorLoader)
+
+    n_ite = 3
+    seed = 10
+    scenario_name = "scenario"
+    actor_name = "actor"
+
+    with patch.object(simulator, "_run_scenario") as mock_run_scenario:
+        mock_run_scenario.return_value = []
+        _ = simulator.run(n_ite, scenario_name, actor_name, {}, [], seed)
+
+    args_list = mock_run_scenario.call_args_list
+
+    assert len(args_list) == n_ite
+    for i, args in enumerate(args_list):
+        expected_current_ite = i
+        expected_seed = seed + i
+        assert args == call(
+            expected_current_ite, scenario_name, actor_name, {}, [], expected_seed
+        )
