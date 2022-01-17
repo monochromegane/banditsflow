cd80014034bbe0174098a9deabbedd0d957eec3a
monochromegane <dev.kuro.obi@gmail.com>
2022-01-17 18:26:37 +0900

    Use simulator in optimizer.

--- banditsflow/optimizer.py
+++ banditsflow/optimizer.py
@@ -3,28 +3,49 @@ from typing import List, Type, cast
 import optuna
 
 from . import actor as act
+from . import scenario
+from . import simulator as sim
 from . import suggestion as suggest
 
 
 class Optimizer:
-    def __init__(self, actor_name: str, loader: Type[suggest.SuggestionLoader]) -> None:
-        self.actor_name = actor_name
-        self.loader = loader
+    def __init__(
+        self,
+        scenario_loader: Type[scenario.ScenarioLoader],
+        actor_loader: Type[act.ActorLoader],
+        suggestion_loader: Type[suggest.SuggestionLoader],
+    ) -> None:
+        self.scenario_loader = scenario_loader
+        self.actor_loader = actor_loader
+        self.suggestion_loader = suggestion_loader
 
     def optimize(
-        self, n_trials: int, n_ite: int, direction: str, metric: str, seed: int
+        self,
+        n_trials: int,
+        n_ite: int,
+        scenario_name: str,
+        actor_name: str,
+        direction: str,
+        metric: str,
+        seed: int,
     ) -> optuna.study.Study:
-        suggestions = self.loader.load(self.actor_name)
+        suggestions = self.suggestion_loader.load(actor_name)
 
         def objective(trial: optuna.trial.Trial) -> float:
             params = suggestions_to_params(suggestions, trial)
 
-            print(params)
-            return 0.0
+            simulator = sim.Simulator(self.scenario_loader, self.actor_loader)
+            results = simulator.run(
+                n_ite, scenario_name, actor_name, params, [], seed + trial.number
+            )
+
+            return to_objective(metric, results)
 
         study = optuna.create_study(
             direction=direction, sampler=optuna.samplers.TPESampler(seed=seed)
         )
+        study.optimize(objective, n_trials=n_trials)
+
         return study
 
 
@@ -75,3 +96,7 @@ def suggestions_to_params(
             )
 
     return params
+
+
+def to_objective(metric: str, results: sim.SimulationResultType) -> float:
+    return 0.0
--- banditsflow/runner.py
+++ banditsflow/runner.py
@@ -1,7 +1,9 @@
 import importlib
-from typing import Any, Dict, cast
+from typing import Any, Dict, Type, cast
 
+from . import actor as act
 from . import optimizer as optim
+from . import scenario
 from . import suggestion as suggest
 
 
@@ -9,14 +11,30 @@ class Runner:
     def __init__(self, scenario_name: str, actor_name: str) -> None:
         self.scenario_name = scenario_name
         self.actor_name = actor_name
+        self.scenario_loader = cast(
+            Type[scenario.ScenarioLoader], importlib.import_module("scenario.loader")
+        )
+        self.actor_loader = cast(
+            Type[act.ActorLoader], importlib.import_module("actor.loader")
+        )
+        self.suggestion_loader = cast(
+            Type[suggest.SuggestionLoader], importlib.import_module("suggestion.loader")
+        )
 
     def optimize(
         self, n_trials: int, n_ite: int, direction: str, metric: str, seed: int
     ) -> Dict[str, Any]:
-        loader = cast(
-            suggest.SuggestionLoader, importlib.import_module("suggestion.loader")
+        optimizer = optim.Optimizer(
+            self.scenario_loader, self.actor_loader, self.suggestion_loader
         )
-        optimizer = optim.Optimizer(self.actor_name, loader)
-        study = optimizer.optimize(n_trials, n_ite, direction, metric, seed)
+        study = optimizer.optimize(
+            n_trials,
+            n_ite,
+            self.scenario_name,
+            self.actor_name,
+            direction,
+            metric,
+            seed,
+        )
 
         return study.best_params
