71716531e46539ec28e36c26a2df775ab9223855
monochromegane <dev.kuro.obi@gmail.com>
2022-01-17 18:57:05 +0900

    Use parameter for optimization.

--- banditsflow/__init__.py
+++ banditsflow/__init__.py
@@ -8,10 +8,21 @@ class BanditsFlow(FlowSpec):  # type: ignore
     param_scenario = Parameter("scenario", type=str, help="Name of scenario")
     param_actor = Parameter("actor", type=str, multiple=True, help="Name of actor")
     param_n_ite = Parameter("n_ite", type=int, default=1, help="Number of simulation")
+    param_seed = Parameter("seed", type=int, default=1, help="Seed of seed")
     param_n_trials = Parameter(
         "n_trials", type=int, default=1, help="Number of optimization"
     )
-    param_seed = Parameter("seed", type=int, default=1, help="Seed of seed")
+    param_optimization_direction = Parameter(
+        "optimization_direction",
+        type=str,
+        default="maximize",
+        help="Name of direction for optimization",
+    )
+    param_optimization_metric = Parameter(
+        "optimization_metric",
+        type=str,
+        help="Name of simulation metric for optimization",
+    )
 
     @step
     def start(self) -> None:
@@ -22,7 +33,11 @@ class BanditsFlow(FlowSpec):  # type: ignore
         actor_name = self.input
         runner = run.Runner(self.param_scenario, actor_name)
         self.best_params = runner.optimize(
-            self.param_n_trials, self.param_n_ite, "maximize", "reward", self.param_seed
+            self.param_n_trials,
+            self.param_n_ite,
+            self.param_optimization_direction,
+            self.param_optimization_metric,
+            self.param_seed,
         )
 
         self.next(self.evaluate)
