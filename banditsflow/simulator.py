37baeb5809542fd9058f98bb0d9cd5a35d0492a8
monochromegane <dev.kuro.obi@gmail.com>
2022-01-17 12:36:51 +0900

    Add callback for each action of scenario.

--- banditsflow/simulator.py
+++ banditsflow/simulator.py
@@ -1,11 +1,16 @@
 import importlib
-from typing import Dict, List, Union, cast
+from typing import Dict, List, Protocol, Union, cast
 
 from . import actor as act
 from . import scenario
 
 ParamsType = Dict[str, Union[None, bool, int, float, str]]
 SimulationResultType = List[List[act.ActionType]]
+
+
+class ActionCallbackType(Protocol):
+    def __call__(self, current_ite: int, step: int, action: act.ActionType) -> None:
+        ...
 
 
 class Simulator:
@@ -19,16 +24,26 @@ class Simulator:
             act.ActorLoader, importlib.import_module("actor.loader")
         )
 
-    def run(self, n_ite: int, params: ParamsType, seed: int) -> SimulationResultType:
+    def run(
+        self,
+        n_ite: int,
+        params: ParamsType,
+        callbacks: List[ActionCallbackType],
+        seed: int,
+    ) -> SimulationResultType:
         results: SimulationResultType = []
         for ite in range(n_ite):
-            result = self._run_scenario(ite, params, seed + ite)
+            result = self._run_scenario(ite, params, callbacks, seed + ite)
             results.append(result)
 
         return results
 
     def _run_scenario(
-        self, current_ite: int, params: ParamsType, seed: int
+        self,
+        current_ite: int,
+        params: ParamsType,
+        callbacks: List[ActionCallbackType],
+        seed: int,
     ) -> List[act.ActionType]:
         scenario = self.scenario_loader.load(self.scenario_name)
         actor = self.actor_loader.load(self.actor_name, params, seed)
@@ -37,6 +52,9 @@ class Simulator:
         while scenario.scan():
             line = scenario.line()
             action = actor.act(line)
+
+            for callback in callbacks:
+                callback(current_ite, len(result), action)
 
             result.append(action)
 
