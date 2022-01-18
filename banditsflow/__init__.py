81bc132dc5cbb8b1e3832b65fa84030c6abfba16
monochromegane <dev.kuro.obi@gmail.com>
2022-01-18 14:15:45 +0900

    Use mlflow for parameter and metric tracking.

--- banditsflow/__init__.py
+++ banditsflow/__init__.py
@@ -1,10 +1,16 @@
-from metaflow import FlowSpec, Parameter, step
+from typing import Dict
+
+import mlflow
+from metaflow import FlowSpec, Parameter, current, step
 from metaflow.datastore.inputs import Inputs
 
+from . import actor as act
 from . import runner as run
 
 
 class BanditsFlow(FlowSpec):  # type: ignore
+    param_git_tag = Parameter("git_tag", type=str, help="Name of Git tag")
+    param_git_commit = Parameter("git_commit", type=str, help="Hash of Git commit")
     param_scenario = Parameter("scenario", type=str, help="Name of scenario")
     param_actor = Parameter("actor", type=str, multiple=True, help="Name of actor")
     param_n_ite = Parameter("n_ite", type=int, default=1, help="Number of simulation")
@@ -45,12 +51,26 @@ class BanditsFlow(FlowSpec):  # type: ignore
     def evaluate(self) -> None:
         actor_name = self.input
         runner = run.Runner(self.param_scenario, actor_name)
-        self.results = runner.evaluate(
-            self.param_n_ite,
-            self.best_params,
-            [],
-            self.param_seed + self.param_n_trials,
-        )
+
+        with mlflow.start_run(
+            experiment_id=self._experiment_id(),
+            run_name=current.run_id,
+            tags=self._experiment_tags(),
+        ):
+            params_for_log = {"scenario": self.param_scenario, "actor": actor_name}
+            params_for_log.update(self.best_params)
+            mlflow.log_params(params_for_log)
+
+            def callback(current_ite: int, step: int, action: act.ActionType) -> None:
+                for key, value in action["metric"].items():
+                    mlflow.log_metric(f"{key}_{current_ite}", value, step=step)
+
+            self.results = runner.evaluate(
+                self.param_n_ite,
+                self.best_params,
+                [callback],
+                self.param_seed + self.param_n_trials,
+            )
 
         self.next(self.join)
 
@@ -65,3 +85,22 @@ class BanditsFlow(FlowSpec):  # type: ignore
     @step
     def end(self) -> None:
         pass
+
+    def _experiment_id(self) -> str:
+        experiment_name = self.param_git_tag
+
+        experiment = mlflow.get_experiment_by_name(experiment_name)
+        experiment_id: str
+        if experiment is not None:
+            experiment_id = experiment.experiment_id
+        else:
+            experiment_id = mlflow.create_experiment(experiment_name)
+
+        return experiment_id
+
+    def _experiment_tags(self) -> Dict[str, str]:
+        return {
+            "step": current.step_name,
+            "git_tag": self.param_git_tag,
+            "git_commit": self.param_git_commit,
+        }
