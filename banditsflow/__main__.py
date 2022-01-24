8e42a5def84ad958bdbd8aeed25d4304e31d8ccc
monochromegane <dev.kuro.obi@gmail.com>
2022-01-24 15:07:13 +0900

    Make it possible to call builder from main.

---
+++ banditsflow/__main__.py
@@ -0,0 +1,24 @@
+import os
+
+import click
+
+from . import scaffold as s
+
+
+@click.group()
+def cli() -> None:
+    pass
+
+
+@click.command()
+@click.argument("name")
+def scaffold(name: str) -> None:
+    s.Builder().build(
+        name, os.path.join(os.getcwd(), "templates"), os.path.dirname(__file__)
+    )
+
+
+cli.add_command(scaffold)
+
+if __name__ == "__main__":
+    cli()
